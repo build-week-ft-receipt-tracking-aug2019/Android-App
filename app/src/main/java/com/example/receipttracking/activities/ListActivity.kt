359863e2ac2b5774a0813231462b0092f0da3008
ronniedipple <ronniedipple@gmail.com>
2019-08-27 00:19:02 -0500

    Added Ui elements to list view

--- app/src/main/java/com/example/receipttracking/activities/ListActivity.kt
+++ app/src/main/java/com/example/receipttracking/activities/ListActivity.kt
@@ -3,11 +3,18 @@ package com.example.receipttracking.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.receipttracking.R
+import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+
+        button.setOnClickListener {
+
+
+        }
     }
 }
